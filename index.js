a4e5fcb2d54d0dd7bf446d2858d97c6fd7a49737
Osvaldo Júnior <osvaldojunior95@gmail.com>
2022-04-24 23:45:03 -0300

    feat: adicionado requisito bônus GET /tweets/USERNAME

--- index.js
+++ index.js
@@ -29,10 +29,17 @@ app.post("/tweets", (req, res) => {
 });
 
 app.get("/tweets", (req, res) => {
-    res.send(tweets.slice(Math.max(tweets.length-10,0)).map(tweet => {
-        tweet.avatar = users.find(user => user.username === tweet.username).avatar;
-        return tweet;
+    res.send(tweets.slice(Math.max(tweets.length - 10, 0)).map(tweet => {
+        return { ...tweet, avatar: users.find(user => user.username === tweet.username).avatar };
     }));
+});
+
+app.get("/tweets/:username", (req, res) => {
+    const { username } = req.params;
+    const list = tweets.filter(tweet => tweet.username === username).map(tweet => {
+        return { ...tweet, avatar: users.find(user => user.username === username).avatar };
+    });
+    res.send(list);
 });
 
 app.listen(5000, () => {
